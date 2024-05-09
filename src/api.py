73ddf1a7c92dcecb4a309df3c3be3d9f728e7c1f
nessapatino <147979709+nessapatino@users.noreply.github.com>
2024-05-09 22:32:59 +0200

    as

--- src/api.py
+++ src/api.py
@@ -24,7 +24,7 @@ def hello_with_image_and_button():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Bienvenido a la API de predicción del síndrome metabólico</title>
+        <title>Bienvenido a la API de predicción del síndrome metabólico...</title>
         <style>
             body {
                 background-image: url('/imagen');
