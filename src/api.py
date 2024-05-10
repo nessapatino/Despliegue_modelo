658a848f886ac89fb20cbcb8d91feda6f9a420d4
nessapatino <147979709+nessapatino@users.noreply.github.com>
2024-05-10 22:08:20 +0200

    update

--- src/api.py
+++ src/api.py
@@ -72,7 +72,7 @@ def hello_with_image_and_button():
     </head>
     <body>
         <div class="content">
-            <h1>PREDICCIÓN DE SÍNDROME METÁBOLICO</h1>
+            <h1>PREDICCIÓN DE SÍNDROME METÁBOLICO..</h1>
             <a href="/predict"><button>Ir al formulario de predicción</button></a>
         </div>
     </body>
